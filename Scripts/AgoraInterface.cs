6b40a975ec56c023bdd5bc4ad39ab82a7fb49b72
Md.Mustafizur Rahman <33628705+MustafizurRU@users.noreply.github.com>
2021-03-20 10:51:43 +0600

    Add files via upload

---
+++ Scripts/AgoraInterface.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using agora_gaming_rtc;
+
+public class AgoraInterface : MonoBehaviour
+{
+    private static string appId = "aacbc6bff87f4f57ba6430dfb6902066";
+    public IRtcEngine mrtcEngine;
+    public uint mRemotePeer;
+
+    public void loadEngine()
+    {
+        //start sdk
+        Debug.Log("Initializing Engine");
+        if(mrtcEngine != null)
+        {
+            Debug.Log("Engine already Exits...please unload");
+            return;
+        }
+        //init rtc engine
+        mrtcEngine = IRtcEngine.GetEngine(appId);
+        //set log level
+        mrtcEngine.SetLogFilter(LOG_FILTER.DEBUG);
+
+
+    }
+    public void joinChannel(string channelName)
+    {
+        Debug.Log("Joinning Channel :"+channelName);
+        if(mrtcEngine == null)
+        {
+            Debug.Log("Initialize Engine before joining channel");
+            return;
+        }
+        //set callbacks
+        mrtcEngine.OnJoinChannelSuccess = OnJoinChannelSuccess;
+        mrtcEngine.OnUserJoined = OnUserJoined;
+        mrtcEngine.OnUserOffline = OnUserJoined;
+        //enable video
+        mrtcEngine.EnableVideo();
+        //enable camera output callback
+        mrtcEngine.EnableVideoObserver();
+        //join the channel
+        mrtcEngine.JoinChannel(channelName, null, 0);
+    }
+    public void leaveChannel()
+    {
+        Debug.Log("Leaving Channel");
+        if (mrtcEngine == null)
+        {
+            Debug.Log("Initialize Engine before leaving channel");
+            return;
+        }
+        //leave channel
+        mrtcEngine.LeaveChannel();
+
+        //disable camera
+        mrtcEngine.DisableVideoObserver();
+    }
+    public void unLoadEngine()
+    {
+        Debug.Log("Unloading Engine");
+        if(mrtcEngine != null)
+        {
+            IRtcEngine.Destroy();
+            mrtcEngine = null;
+        }
+    }
+    //implement engine callbacks
+    private void OnJoinChannelSuccess(string channelName,uint uid, int elapsed)
+    {
+        Debug.Log("Successfully joined channel " + channelName + "with id= " + uid);
+    }
+    private void OnUserJoined(uint uid, int elapsed)
+    {
+        Debug.Log("New user has joined channel with id = " + uid);
+    }
+    private void OnUserJoined()
+    {
+
+    }
+
+}
---
+++ Scripts/ButtonHandle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandle : MonoBehaviour
+{
+   public void onClickButton()
+    {
+        Debug.Log("Button Name is : " + name);
+        ///GameObject go = GameObject.Find("NameInput");
+       /// InputField inputName = go.GetComponent<InputField>();
+      ///  Debug.Log("Your Name is \' " + inputName.text + "\' ");
+      if(name.CompareTo("JoinButton") == 0)
+        {
+            onJoinButtonClicked();
+        }
+      else if(name.CompareTo("LeaveButton") == 0)
+        {
+            onLeaveButtonClicked();
+
+        }
+
+
+    }
+    private void onJoinButtonClicked()
+    {
+        Debug.Log("You Are In");
+        GameObject go = GameObject.Find("NameInput");
+        InputField inputName = go.GetComponent<InputField>();
+    }
+    private void onLeaveButtonClicked()
+    {
+        Debug.Log("You Are Out");
+       
+    }
+}
